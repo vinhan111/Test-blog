97bcdfeb0cdcea0c0f34dd7faa90caa634efa78b
vinhan111 <darkmager@gmail.com>
2025-08-05 10:15:22 +0700

    change back to output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-   output: 'static',
+   output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
