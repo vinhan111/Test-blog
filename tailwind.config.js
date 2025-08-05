358026fc7aa7939caf357a1ff43e5f884f18dfbb
vinhan111 <darkmager@gmail.com>
2025-08-05 11:28:24 +0700

    git update some pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
-import { fontFamily } from 'tailwindcss/defaultTheme'
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-sans)', ...fontFamily.sans],
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      colors: {
+        base: "#000000",
+        text: "#e9e4d4",
+        accent: "#c3ab62",
+        subtext: "#bcae85",
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
-}
+
+  plugins: [require("@tailwindcss/typography")],
+};
