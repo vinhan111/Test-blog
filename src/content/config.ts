df6dad31dac98df5ba5ee3d5b9c49a586952897c
vinhan111 <darkmager@gmail.com>
2025-08-05 16:39:43 +0700

    pnpm in use

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,22 +2,26 @@ import { glob } from "astro/loaders";
 import { z, reference, defineCollection } from "astro:content";
 
 const articles = defineCollection({
-    loader: glob({ pattern: ["**/*.md", "**/*.mdx"], base: "./src/content/articles" }),
-    schema: ({ image }) => z.object({
-        cover: image(),
-        coverAlt: z.string(),
-        title: z.string(),
-        slug: z.string(),
-        snippet: z.string(),
-        category: z.string(),
-        pubDate: z.coerce.date(),
-        readingDuration: z.number(),
-        originalLink: z.string().url(),
-        isDraft: z.boolean().default(false),
-        updatedDate: z.coerce.date().optional(),
-        author: z.string().default('Retro Rocket Team'),
-        relatedArticles: z.array(reference('articles')).optional(),
-        mermaid: z.boolean().default(false)
+  loader: glob({
+    pattern: ["**/*.md", "**/*.mdx"],
+    base: "./src/content/articles",
+  }),
+  schema: ({ image }) =>
+    z.object({
+      cover: image(),
+      coverAlt: z.string(),
+      title: z.string(),
+      slug: z.string(),
+      snippet: z.string(),
+      category: z.string(),
+      pubDate: z.coerce.date(),
+      readingDuration: z.number(),
+      originalLink: z.string().url(),
+      isDraft: z.boolean().default(false),
+      updatedDate: z.coerce.date().optional(),
+      author: z.string().default("Retro Rocket Team"),
+      relatedArticles: z.array(reference("articles")).optional(),
+      mermaid: z.boolean().default(false),
     }),
 });
 
